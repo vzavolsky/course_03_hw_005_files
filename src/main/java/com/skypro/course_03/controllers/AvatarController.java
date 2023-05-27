0c453ea1927580b3f2b5b004029f585865bfd81d
vzavolsky <vzavolsky@ya.ru>
2023-05-28 00:45:01 +0300

    Course 03 - HW 005 - files v2

---
+++ src/main/java/com/skypro/course_03/controllers/AvatarController.java
@@ -0,0 +1,25 @@
+package com.skypro.course_03.controllers;
+
+import com.skypro.course_03.services.AvatarService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("avatar")
+public class AvatarController{
+
+    private AvatarService avatarService;
+
+    public AvatarController(AvatarService avatarService) {
+        this.avatarService = avatarService;
+    }
+
+    @PostMapping()
+    public ResponseEntity<String> upload(@RequestParam Long studentId, @RequestParam MultipartFile multipartFile) {
+        return ResponseEntity.ok()
+    }
+}
---
+++ src/main/java/com/skypro/course_03/services/AvatarService.java
@@ -0,0 +1,7 @@
+package com.skypro.course_03.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AvatarService {
+}
